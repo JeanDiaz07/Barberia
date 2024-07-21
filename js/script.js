75a4bb683ed12692f39a9629a265a9e4932c09f4
JeanDiaz07 <jean.diaz0907@gmail.com>
2024-07-21 18:23:50 -0400

    cambios de diseño

--- js/script.js
+++ js/script.js
@@ -9,32 +9,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Función para generar el calendario
     function generateCalendar() {
-        let calendarHTML = "<table class='table table-bordered'><thead><tr><th>Día/Hora</th>"; // Encabezado de la tabla
+        let calendarHTML = "<table class='table table-bordered'><thead><tr><th>Hora/Día</th>";
 
-        // Generar los encabezados de las horas
-        for (let hour = 10; hour < 21; hour++) {
-            calendarHTML += "<th>" + hour + ":00</th>";
+        // Generar los encabezados de las fechas
+        for (let day = 0; day < 6; day++) {
+            const currentDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + day);
+            calendarHTML += "<th>" + currentDate.toLocaleDateString() + "</th>";
         }
         calendarHTML += "</tr></thead><tbody>";
 
-        // Generar las filas de la tabla para cada día de la semana
-        for (let day = 0; day < 6; day++) {
-            const currentDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + day);
-            const dateString = currentDate.toISOString().split('T')[0]; // Obtener la fecha en formato YYYY-MM-DD
-            
-            calendarHTML += "<tr><td>" + currentDate.toLocaleDateString() + "</td>";
-          
-            // Generar las celdas de la tabla para cada hora del día
-            for (let hour = 10; hour < 21; hour++) {
-                const timeString = hour.toString().padStart(2, '0') + ":00";
-                console.log(timeString);
+        // Generar las filas de la tabla para cada hora del día
+        for (let hour = 10; hour < 21; hour++) {
+            const timeString = hour.toString().padStart(2, '0') + ":00";
+            calendarHTML += "<tr><td>" + timeString + "</td>";
+
+            // Generar las celdas de la tabla para cada día de la semana
+            for (let day = 0; day < 6; day++) {
+                const currentDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + day);
+                const dateString = currentDate.toISOString().split('T')[0];
+
                 // Verifica si hay una reserva para la hora y fecha específica
                 let isOccupied = reservations.some(res => res.Fecha === dateString && res.Hora === timeString);
             
                 // Añade el resultado al calendario HTML
                 calendarHTML += `<td class='${isOccupied ? 'occupied' : 'available'}'>${isOccupied ? 'Reservado' : 'Disponible'}</td>`;
             }
-            
+
             calendarHTML += "</tr>";
         }
 
